66f2af58f2c002e21b264af70c74c6a821a0c8be
Chimwemwe Masona <chimmasona@gmail.com>
2022-11-14 16:10:04 +0200

    created files and added images

---
+++ src/Pages/Front Page.js
@@ -0,0 +1,12 @@
+import "../Images/metabnb.png"
+import React from "react"
+
+function First() {
+    return (
+        <div>
+            <img src="../Images/metabnb.png"></img>
+        </div>
+    )
+}
+
+export default First
