71820ac93e70208d92112a04c3e97876580ee199
Chimwemwe Masona <chimmasona@gmail.com>
2022-11-15 11:36:49 +0200

    almost completed the navigation bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,26 @@
 import React from "react";
-import logo1 from "../Images/Home/Navbar/logo1.png"
-
-
+import logo1 from "../Images/Home/Navbar/logo1.png";
 
 function Navbar() {
-    return (
-        <nav>
-            <img src={logo1} />
-        </nav>
-    )
+  return (
+    <nav>
+      <img src={logo1} />
+      <div className="nav-buttons">
+        <div className="links">
+
+            {/* Come back and redo anchor tags */}
+          <a>Home</a>
+          <a>Place to stay</a>
+          <a>NFTs</a>
+          <a>Community</a>
+        </div>
+
+        <div>
+          <button>Connect Wallet</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
