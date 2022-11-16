eec4d97d67eaa5d0eb6194cb18143c7ee91deb47
Chimwemwe Masona <chimmasona@gmail.com>
2022-11-16 17:06:41 +0200

    Started styling the Cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import card1 from "../Images/Home/Frame 151.png";
+import stars from "../Images/Home/stars.png";
+
 function Cards() {
     return (
         <div className="cards-header">
@@ -7,7 +10,23 @@ function Cards() {
                 <p>Inspiration for your next adventure</p>
             </div>
             <div className="cards">
+                <div className="cards-row-1">
+                    <div className="card1">
+                        <img src={card1}/>
+                        <div className="card-text">
+                            <p>Desert king</p>
+                            <p><b>1MBT per night</b></p>
+                        </div>
 
+                        <div className="card-text-2">
+                            <p>2345km away</p>
+                            <p>available for 2weeks stay</p>
+                        </div>
+
+                        <img src={stars}/>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
